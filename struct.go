1690a42dac6c77033bc5c22e28795267e6602608
CC11001100 <CC11001100@qq.com>
2022-12-02 00:17:34 +0800

    add todo

--- struct.go
+++ struct.go
@@ -1,30 +1,27 @@
 package delta
 
-import (
-	"reflect"
-)
-
-// ZipStructSlice 对Struct序列进行压缩
-func ZipStructSlice[T any](slice []T) {
-
-}
-
-func diff[T any](a, b T) *StructDelta {
-	reflectA := reflect.ValueOf(a)
-	reflectB := reflect.ValueOf(b)
-	
-	switch reflectA.Type().Kind() {
-	case reflect.String:
-
-	}
-}
-
-type StructDelta struct {
-	Init          any
-	FieldDeltaMap map[string]*FieldDelta
-}
-
-type FieldDelta struct {
-	FieldName  string
-	FieldValue string
-}
+// TODO 2022-12-2 00:17:18 struct的diff对比
+//// ZipStructSlice 对Struct序列进行压缩
+//func ZipStructSlice[T any](slice []T) {
+//
+//}
+//
+//func diff[T any](a, b T) *StructDelta {
+//	reflectA := reflect.ValueOf(a)
+//	reflectB := reflect.ValueOf(b)
+//
+//	switch reflectA.Type().Kind() {
+//	case reflect.String:
+//
+//	}
+//}
+//
+//type StructDelta struct {
+//	Init          any
+//	FieldDeltaMap map[string]*FieldDelta
+//}
+//
+//type FieldDelta struct {
+//	FieldName  string
+//	FieldValue string
+//}
