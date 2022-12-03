34ed55a691f2e9f8df91174d450e498bbd567187
CC11001100 <CC11001100@qq.com>
2022-12-03 13:41:43 +0800

    重构time delta，待测试

--- time.go
+++ time.go
@@ -1,81 +1,97 @@
 package delta
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // ------------------------------------------------ ---------------------------------------------------------------------
 
-// TimeSliceDelta 对时间序列做delta
-func TimeSliceDelta(timeSlice []time.Time, compareToType CompareToType) []uint64 {
-	result := make([]uint64, len(timeSlice))
+// ToTimeSliceDelta 对时间序列做delta
+func ToTimeSliceDelta(timeSlice []time.Time, compareToType CompareToType) []uint64 {
+	deltaSlice := make([]uint64, len(timeSlice))
 	for index, t := range timeSlice {
 		if index == 0 {
-			result[index] = uint64(t.UnixMilli())
+			deltaSlice[index] = uint64(t.UnixMilli())
 			continue
 		}
-		if compareToType == CompareToFirst {
-			result[index] = TimeDelta(t, timeSlice[0])
-		} else if compareToType == CompareToLast {
-			result[index] = TimeDelta(t, timeSlice[index-1])
+		switch compareToType {
+		case CompareToFirst:
+			deltaSlice[index] = TimeDelta(t, timeSlice[0])
+		case CompareToLast:
+			deltaSlice[index] = TimeDelta(t, timeSlice[index-1])
+		default:
+			panic(fmt.Errorf("not support compare type: %#v", compareToType))
 		}
 	}
-	return result
+	return deltaSlice
 }
 
 // FromTimeSliceDelta 从Time的delta恢复时间序列
 func FromTimeSliceDelta(deltaSlice []uint64, compareToType CompareToType) []time.Time {
-	result := make([]time.Time, 0)
+	timeSlice := make([]time.Time, 0)
 	for index, delta := range deltaSlice {
 		if index == 0 {
-			result[index] = time.UnixMilli(int64(delta))
+			timeSlice[index] = time.UnixMilli(int64(delta))
 			continue
 		}
-		if compareToType == CompareToFirst {
-			result[index] = time.UnixMilli(int64(deltaSlice[0] + delta))
-		} else if compareToType == CompareToLast {
-			result[index] = result[index-1].Add(time.Millisecond * time.Duration(delta))
+		switch compareToType {
+		case CompareToFirst:
+			timeSlice[index] = time.UnixMilli(int64(deltaSlice[0] + delta))
+		case CompareToLast:
+			timeSlice[index] = timeSlice[index-1].Add(time.Millisecond * time.Duration(delta))
+		default:
+			panic(fmt.Errorf("not support compare type: %#v", compareToType))
 		}
 	}
-	return result
+	return timeSlice
 }
 
+// TimeDelta 求两个时间的差
 func TimeDelta(t1 time.Time, t2 time.Time) uint64 {
 	return uint64(t1.UnixMilli() - t2.UnixMilli())
 }
 
 // ------------------------------------------------ ---------------------------------------------------------------------
 
-// TimestampSliceDelta 对时间戳做delta
-func TimestampSliceDelta(timestampSlice []uint64, compareToType CompareToType) []uint64 {
-	result := make([]uint64, len(timestampSlice))
+// ToTimestampSliceDelta 对时间戳做delta
+func ToTimestampSliceDelta(timestampSlice []uint64, compareToType CompareToType) []uint64 {
+	deltaSlice := make([]uint64, len(timestampSlice))
 	for index, timestamp := range timestampSlice {
 		if index == 0 {
-			result[index] = timestamp
+			deltaSlice[index] = timestamp
 			continue
 		}
-		if compareToType == CompareToFirst {
-			result[index] = timestampSlice[index] - timestampSlice[0]
-		} else if compareToType == CompareToLast {
-			result[index] = timestampSlice[index] - timestampSlice[index-1]
+		switch compareToType {
+		case CompareToFirst:
+			deltaSlice[index] = timestamp - timestampSlice[0]
+		case CompareToLast:
+			deltaSlice[index] = timestamp - timestampSlice[index-1]
+		default:
+			panic(fmt.Errorf("not support compare type: %#v", compareToType))
 		}
 	}
-	return result
+	return deltaSlice
 }
 
 // FromTimestampSliceDelta 从时间戳的delta恢复时间戳序列
 func FromTimestampSliceDelta(deltaSlice []uint64, compareToType CompareToType) []uint64 {
-	result := make([]uint64, 0)
+	timeSlice := make([]uint64, 0)
 	for index, delta := range deltaSlice {
 		if index == 0 {
-			result[index] = delta
+			timeSlice[index] = delta
 			continue
 		}
-		if compareToType == CompareToFirst {
-			result[index] = deltaSlice[0] + delta
-		} else if compareToType == CompareToLast {
-			result[index] = result[index-1] + delta
+		switch compareToType {
+		case CompareToFirst:
+			timeSlice[index] = deltaSlice[0] + delta
+		case CompareToLast:
+			timeSlice[index] = timeSlice[index-1] + delta
+		default:
+			panic(fmt.Errorf("not support compare type: %#v", compareToType))
 		}
 	}
-	return result
+	return timeSlice
 }
 
 // ------------------------------------------------ ---------------------------------------------------------------------
---
+++ time_test.go
@@ -0,0 +1,29 @@
+package delta
+
+import (
+	"testing"
+)
+
+func TestFromTimeSliceDelta(t *testing.T) {
+	// TODO
+	
+}
+
+func TestFromTimestampSliceDelta(t *testing.T) {
+	// TODO
+
+}
+
+func TestTimeDelta(t *testing.T) {
+	// TODO
+
+}
+
+func TestTimeSliceDelta(t *testing.T) {
+
+	// TODO
+}
+
+func TestTimestampSliceDelta(t *testing.T) {
+	// TODO
+}
